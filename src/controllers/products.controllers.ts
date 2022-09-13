14791868b0e32465b9509ee791ac60fbdbdfa3a9
Guilherme Eduardo <guigui.carvalho15@gmail.com>
2022-09-13 17:12:51 -0300

    adicionando rota para adicionar novas armas

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -4,8 +4,8 @@ import ProductsService from '../services/products.services';
 export default class ProductsController {
   constructor(private productService = new ProductsService()) {}
 
-  public async create(req: Request, res: Response): Promise<Response> {
+  create = async (req: Request, res: Response): Promise<Response> => {
     const product = await this.productService.create(req.body);
     return res.status(201).json(product);
-  }
+  };
 }
--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,23 +1,16 @@
-import { Pool, ResultSetHeader } from 'mysql2';
+import { ResultSetHeader } from 'mysql2';
 import ProductBody from '../interfaces/product.body.interface';
-// import connection from './connection';
+import connection from './connection';
 
 export default class ProductModel {
-  public connection: Pool;
-
-  constructor(connection: Pool) {
-    this.connection = connection;
-  }
-
-  public async create(body: ProductBody): Promise<ProductBody> {
+  create = async (body: ProductBody): Promise<ProductBody> => {
     const { name, amount } = body;
-    const result = await this.connection.execute<ResultSetHeader>(
+    const result = await connection.execute<ResultSetHeader>(
       'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
       [name, amount],
     );
-    console.log(result);
     const [data] = result;
     const { insertId } = data;
     return { id: insertId, name, amount };
-  }
+  };
 }
