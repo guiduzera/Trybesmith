40aa530e1c3f99920ec93a79e10ce851007a2be2
Guilherme Eduardo <guigui.carvalho15@gmail.com>
2022-09-14 14:04:47 -0300

    fazendo teste de estrutura de classe

--- src/controllers/orders.controllers.ts
+++ src/controllers/orders.controllers.ts
@@ -3,10 +3,14 @@ import 'express-async-errors';
 import OrdersServices from '../services/orders.services';
 
 export default class OrdersControllers {
-  constructor(private ordersServices = new OrdersServices()) {}
+  public sercive: OrdersServices;
+
+  constructor() {
+    this.sercive = new OrdersServices();
+  }
 
   getAll = async (_req: Request, res: Response): Promise<Response> => {
-    const orders = await this.ordersServices.getAll();
+    const orders = await this.sercive.getAll();
     return res.status(200).json(orders);
   };
 }
